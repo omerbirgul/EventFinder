e362e237777e94cd0cafdac90557d3b54c2324bd
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-02-23 01:30:31 +0300

    IUserRepository added

--- EvenFinder/EvenFinder/Data2/Abstract/IUserRepository.cs
+++ EvenFinder/EvenFinder/Data2/Abstract/IUserRepository.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using EvenFinder.Data;
+
+namespace EvenFinder.Data2.Abstract
+{
+    public interface IUserRepository
+    {
+        IQueryable<User> Users { get; }
+
+
+        void CreateUser(User user);
+    }
+}
