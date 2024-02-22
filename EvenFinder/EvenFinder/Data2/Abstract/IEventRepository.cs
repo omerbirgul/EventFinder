4a04004e5136f6b56027728c636c4d2b03aa2efe
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-02-23 01:31:00 +0300

    EfUserRepositoryAdded

--- EvenFinder/EvenFinder/Data2/Abstract/IEventRepository.cs
+++ EvenFinder/EvenFinder/Data2/Abstract/IEventRepository.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using EvenFinder.Data;
+
+namespace EvenFinder.Data2.Abstract
+{
+    public interface IEventRepository
+    {
+        IQueryable<Event> Events { get; }
+
+        void CreateEvent(Event post);
+    }
+}
