f23cc3f01183d72b01138a647370b5ab016eb63a
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-02-23 18:28:38 +0300

    RegisterViewModel added

---
+++ EvenFinder/EvenFinder/Models/RegisterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvenFinder.Models
+{
+    public class RegisterViewModel
+    {
+
+        [Required]
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+        public string? LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Image")]
+        public string? Image { get; set; }
+
+        [Required]
+        [Display(Name = "UserName")]
+        public string? UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "RepeatPassword")]
+        [Compare(nameof(Password), ErrorMessage ="Passwords doesn't match!")]
+        public string? ConfirmPassword { get; set; }
+
+
+    }
+}
