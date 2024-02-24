955d89fc2c35a18f91b3a7cdba7f02243aa04e38
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2024-02-24 14:41:27 +0300

    IRegistrationRepository added

---
+++ EvenFinder/EvenFinder/Data2/Abstract/IRegistertrationRepository.cs
@@ -0,0 +1,11 @@
+﻿using EvenFinder.Data;
+
+namespace EvenFinder.Data2.Abstract
+{
+    public interface IRegistrationRepository
+    {
+        IQueryable<Registration> Registrations { get; }
+
+        void Register(Registration registration);
+    }
+}
